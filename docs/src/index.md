cc070225ede3b5963c74fad5c0f24f2278e98dfa
Neel Smith <nsmith@Technites.local>
2021-03-30 08:36:05 -0400

    Outline implementation of MID orthography

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,21 @@
 # AtticGreek
 
 *A rigorously defined orthography for texts in the archaic Attic alphabet*.
+
+Highlights:
+
+- implement the MID Orthography interface, with semantic tokenization of text in Attic Greek
+- uses Unicode in form `:NFKC` whererever codepoints are defined
+- mimics print publication practice insupplementing missing characters with the ASCII code point sumarized below.
+
+
+| Code point | Meaning |
+| --- | --- |
+| h | rough breathing |
+| ê | ε with circumflex |
+| ô | ο with circuflex |
+
+    
+!!! note
+    In the Attic alphabet, all iotas are adscript.
+    There is no character for smooth breathing:  the aspirate (rough breathing) is explicitly marked, so that word-initial vowels are unaspirated by default.
--- test/test_orthography.jl
+++ test/test_orthography.jl
@@ -2,3 +2,41 @@
 @testset "Test creating AtticGreek structure" begin
     
 end
+
+@testset "Test generating alphabetic characters" begin
+    alphas = PolytonicGreek.alphabetic()
+    #expectedcount = 267
+    #@test length(alphas) == expectedcount
+end
+
+@testset "Test generating puncutation characters" begin
+    puncts = PolytonicGreek.punctuation()
+    @test puncts == ".,;:"
+end
+
+@testset "Test recognizing alphabetic tokens" begin
+    @test PolytonicGreek.isAlphabetic("hο δêμος")
+end
+
+@testset "Test recognizing punctuation tokens" begin
+    @test PolytonicGreek.isPunctuation(":")
+end
+
+@testset "Test creating token from string value" begin
+    t = PolytonicGreek.tokenforstring("μῆνιν")
+    typeof(t.tokencategory) == Orthography.LexicalToken
+    t.text == "μῆνιν"
+end
+
+@testset "Test splitting punctuation from alphabetic" begin
+    split = PolytonicGreek.splitPunctuation("ἄειδε,")
+    @test length(split) == 2
+    @test split[1] == "ἄειδε"
+    @test split[2] == ","
+end
+
+@testset "Test tokenizing Attic Greek" begin
+    tokens = PolytonicGreek.tokenizeLiteraryGreek("μῆνιν ἄειδε,")
+    @test length(tokens) == 3
+    @test tokens[end].text == ","
+end
