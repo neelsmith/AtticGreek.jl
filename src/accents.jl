15057b65c3b2fc46e59277052a9edb3e933ae9b2
Neel Smith <dnsmith.neel@gmail.com>
2021-05-11 17:23:12 -0400

    Functions to extract syllables from words

--- src/accents.jl
+++ src/accents.jl
@@ -17,3 +17,88 @@
         end
         join(stripped,"")
     end
+
+
+    function addacute(vowel::AbstractString; ortho::AtticOrthography)
+        bare = PolytonicGreek.stripquant(vowel)
+        dict = acutedict(ortho)
+        if bare in keys(dict)
+            accented = dict[bare]
+            if occursin("_", vowel)
+                string(accented,"_")
+            else
+                accented
+            end
+        else
+            @warn("addacute: can't add acute accent to string $vowel")
+            nothing
+        end
+    end
+
+
+    function addcircumflex(vowel::AbstractString; ortho::AtticOrthography)
+        bare = PolytonicGreek.stripquant(vowel)
+        dict = circumflexdict(ortho) 
+        if bare in keys(dict)
+            accented = dict[bare]
+            if occursin("_", vowel)
+                string(accented,"_")
+            else
+                accented
+            end
+        else
+            @warn("addcircumflex: can't add circumflex accent to string $vowel")
+            nothing
+        end
+    end
+
+
+
+
+
+"""Return ultima.
+
+$(SIGNATURES)
+"""
+function ultima(s, ag::AtticOrthography)
+    
+    sylls = syllabify(s, ag)
+    if isempty(sylls)
+        @warn("ultima: no syllables in string $s")
+        nothing
+    else
+        sylls[end]
+    end
+end
+
+
+"""Return penult.
+
+$(SIGNATURES)
+"""
+function penult(s, ag::AtticOrthography)
+    sylls = syllabify(s, ag)
+    if length(sylls) < 2
+        @warn("penult: cannot extract penult from word with < 2 syllables: $s")
+        nothing
+    else
+        sylls[end - 1]
+    end
+end
+
+"""Return antepenult.
+
+$(SIGNATURES)
+
+NB: Rms accent since they're not relevant to syllables.
+"""
+function antepenult(s, ag::AtticOrthography)
+    sylls = syllabify(s, ag)
+    if length(sylls) < 3
+        @warn("penult: cannot extract antepenult from word with < 3 syllables: $s")
+        nothing
+    else
+        sylls[end - 2]
+    end
+end
+
--- test/test_accents.jl
+++ test/test_accents.jl
@@ -3,3 +3,23 @@
     @test rmaccents("τôι", atticGreek()) == nfkc("τοι")
     @test rmaccents("δέμοι", atticGreek()) == nfkc("δεμοι")
 end
+
+@testset "Test adding acute accents to vowels" begin
+    @test PolytonicGreek.addacute("α") == "ά"
+    @test PolytonicGreek.addacute("αυ") == "αύ"
+    @test PolytonicGreek.addacute("ϊ") == "ΐ"
+end
+
+
+@testset "Test adding circumflex accents to vowels" begin
+    @test PolytonicGreek.addcircumflex("α") == "ᾶ"
+    @test PolytonicGreek.addcircumflex("αυ") == "αῦ"
+    @test PolytonicGreek.addcircumflex("ϊ") == "ῗ"
+end
+
+
+
+@testset "Test extracting syllables from words" begin
+    @test PolytonicGreek.antepenult("άνθροπος") == "αν"
+    @test PolytonicGreek.penult("άνθροπος") ==  "θρο"
+end
