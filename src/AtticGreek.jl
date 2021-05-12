17ac6a1bdabef10ce820126dcbd911bb7603def2
Neel Smith <dnsmith.neel@gmail.com>
2021-05-12 16:54:36 -0400

    Import functions to override

--- src/AtticGreek.jl
+++ src/AtticGreek.jl
@@ -7,6 +7,9 @@ import Orthography: codepoints
 import Orthography: tokentypes
 
 using PolytonicGreek
+import PolytonicGreek: syllabify
+import PolytonicGreek: accentword
+import PolytonicGreek: sortWords
 import PolytonicGreek: vowels
 import PolytonicGreek: consonants
 import PolytonicGreek: rmaccents
@@ -16,7 +19,7 @@ using Documenter, DocStringExtensions
 export AtticOrthography
 export atticGreek, vowels, consonants
 export tokenizeAtticGreek
-export sortWords
+#export sortWords # Need to republish this in Polytonic Greek
 export accentword
 
 export rmaccents
